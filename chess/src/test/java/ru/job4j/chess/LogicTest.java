1fb63f1591c05f7deaaf9b07f2115dc1b056917e
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-04-13 08:02:28 +0300

    3. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -11,37 +11,31 @@ public class LogicTest {
 
     @Test
     public void whenMoveThenFigureNotFoundException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+            throws ImpossibleMoveException {
         Logic logic = new Logic();
-        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
-            logic.move(Cell.C1, Cell.H6);
-        });
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> logic.move(Cell.C1, Cell.H6));
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
 
     @Test
     public void whenMoveThenImpossibleMoveException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+            throws ImpossibleMoveException {
         Logic logic = new Logic();
         BishopBlack bishop = new BishopBlack(Cell.C1);
         logic.add(bishop);
-        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
-            logic.move(Cell.C1, Cell.H1);
-        });
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> logic.move(Cell.C1, Cell.H1));
         assertThat(exception.getMessage()).isEqualTo("Can't move from C1 to H1");
     }
 
     @Test
     public void whenMoveThenOccupiedCellException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+            throws ImpossibleMoveException {
         Logic logic = new Logic();
         BishopBlack bishop1 = new BishopBlack(Cell.C1);
         BishopBlack bishop2 = new BishopBlack(Cell.E3);
         logic.add(bishop1);
         logic.add(bishop2);
-        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
-            logic.move(Cell.C1, Cell.E3);
-        });
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> logic.move(Cell.C1, Cell.E3));
         assertThat(exception.getMessage()).isEqualTo("The cell E3 is already occupied!");
     }
 }
